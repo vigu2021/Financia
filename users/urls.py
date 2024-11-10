6d073c2c180c8a0cb5fb5ed78ad205627da20823
Vignesh Roachthavilit <vigneshroachthavilit@gmail.com>
2024-11-11 00:10:36 +0700

    Made finances app

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 
 urlpatterns = [
-     path('signup/', views.signup_view, name='signup'),
+    path('', views.home, name='home'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
+
+def home(request):
+    return render(request,'home.html')
+
 def signup_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()  
             messages.success(request, f"Account created successfully for {user.username}!")
-            return
+            return redirect('login')
     else:
-        form = UserCreationForm()
+        messages.error(request, "There was an error with your form.")
 
     return render(request, "signup.html")
 
