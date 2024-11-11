205010ec01883f3ee821dfb5acb5ede402367a86
Vignesh Roachthavilit <vigneshroachthavilit@gmail.com>
2024-11-11 11:13:51 +0700

    Made Transaction model

--- finances/models.py
+++ finances/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Transaction(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='transaction')
+    amount = models.FloatField(default = 0)
+    trans_type = models.CharField(max_length=50)
+    is_gain = models.BooleanField()
+
+    class Meta:
+        db_table = 'transactions'
+
+    def __str__(self):
+        return f"{self.user} - {"+" if self.is_gain else "-"}{self.amount} - {self.trans_type}"
+
+
