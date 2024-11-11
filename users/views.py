1920f7f462a34555bad5a102a66ea845afee7309
Vignesh Roachthavilit <vigneshroachthavilit@gmail.com>
2024-11-11 22:58:41 +0700

    Fixed Html to output errors

--- users/views.py
+++ users/views.py
@@ -10,12 +10,14 @@ def signup_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()  
+            user = form.save()
             messages.success(request, f"Account created successfully for {user.username}!")
             return redirect('login')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, error)  # Pass each error to messages for display
     else:
-        messages.error(request, "There was an error with your form.")
+        form = UserCreationForm()
 
-    return render(request, "signup.html")
-
-    
+    return render(request, "signup.html", {"form": form})
